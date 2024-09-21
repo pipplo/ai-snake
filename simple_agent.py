08672720932df47b8933192a6aa3b493de30131c
pipplo <contact@pipplo.com>
2024-09-20 22:21:18 -0700

    Add paramaters to simple_agent and dump qtable

--- simple_agent.py
+++ simple_agent.py
@@ -2,6 +2,8 @@ import random
 import numpy as np
 from collections import defaultdict
 from tqdm import tqdm
+import argparse
+import pickle
 
 from engine import Engine
 from parameters import *
@@ -11,10 +13,10 @@ import pygame
 class SimpleAgent:
     def __init__(
             self,
-            learning_rate,
-            start_epsilon,
-            epsilon_decay,
-            final_epsilon,
+            learning_rate=.005,
+            start_epsilon=1,
+            epsilon_decay=.1,
+            final_epsilon=0,
             discount_factor = .9,
             ):
         
@@ -60,6 +62,22 @@ class SimpleAgent:
 
     def decay_epsilon(self):
         self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
+
+    def load(self, file):
+        with open(file, 'rb') as f:
+            simplified = pickle.load(f)
+            self.q_values.clear()
+            for k,v in simplified.items():
+                self.q_values[k] = v
+
+
+    def save(self, file):
+        simplified = {}
+        for k,v in self.q_values.items():
+            simplified[k] = v
+
+        with open(file, 'wb') as f:
+            pickle.dump(simplified, f)
 
 def train():
     n_episodes = 100000
@@ -109,6 +127,9 @@ def train():
     # print(agent.q_values)
     print((max_score, max_step_count))
 
+    return agent
+
+def play(agent):
     # run a few games with the final result
     for i in range(10):
         # create game engine
@@ -140,9 +161,22 @@ def train():
             game_ui.draw_board(game_engine.board)
             game_ui.draw_score(game_engine.snake_len)
 
-    # print(agent.q_values)
-    print((max_score, max_step_count))
-
 # a default way to run the agent to train itself
 if __name__ == '__main__':
-    train()
+
+    parser = argparse.ArgumentParser(description="Run a simple Q-Table agent")
+    parser.add_argument('--play', dest='play', action='store_false', help='Play the agent')
+    parser.add_argument('--train', dest='train', action='store_true', help='train the model')
+    parser.add_argument('--load', dest='load_file', action='store', default='qtable/qtable.pkl', help='specified file to load', )
+    args = parser.parse_args()
+
+    if (args.train):
+        agent = train()
+        agent.save(args.load_file)
+    
+    agent = SimpleAgent()
+
+    agent.load(args.load_file)
+
+    if(args.play):
+        play(agent)
